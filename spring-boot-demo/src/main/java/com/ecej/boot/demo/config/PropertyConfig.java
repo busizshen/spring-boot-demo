b7b9c45676cd6aa4024ac0eb82c363e199b779bf
QIANGLU <598505651@qq.com>
2017-01-04 16:42:18 +0800

    增加基础dubbo扫描配置，解决扫描不到的问题

--- spring-boot-demo/src/main/java/com/ecej/boot/demo/config/PropertyConfig.java
+++ spring-boot-demo/src/main/java/com/ecej/boot/demo/config/PropertyConfig.java
@@ -1,6 +1,7 @@
 package com.ecej.boot.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @PropertySource(ignoreResourceNotFound = true, value = { "classpath:remote-db.properties",
 		"classpath:remote-dubbo.properties", "classpath:remote-mq.properties", "classpath:remote-redis.properties",
 		"classpath:remote-zk.properties" })
+@ImportResource({ "classpath:dubbo/*.xml" })
 public class PropertyConfig extends WebMvcConfigurerAdapter {
 
 }
--- spring-boot-demo/src/main/java/com/ecej/boot/demo/run/Startup.java
+++ spring-boot-demo/src/main/java/com/ecej/boot/demo/run/Startup.java
@@ -1,11 +1,9 @@
-package com.ecej.boot.demo;
+package com.ecej.boot.demo.run;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource({ "classpath:dubbo/*.xml" })
 public class Startup {
 
 	public static void main(String[] args) {
