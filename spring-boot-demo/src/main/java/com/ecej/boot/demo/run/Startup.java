fe81978f878c7751c7edb482747b1ee781819031
QIANGLU <598505651@qq.com>
2017-01-04 17:43:22 +0800

    增加扫描支持

--- spring-boot-demo/src/main/java/com/ecej/boot/demo/run/Startup.java
+++ spring-boot-demo/src/main/java/com/ecej/boot/demo/run/Startup.java
@@ -2,8 +2,10 @@ package com.ecej.boot.demo.run;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.ecej.boot")
 public class Startup {
 
 	public static void main(String[] args) {
